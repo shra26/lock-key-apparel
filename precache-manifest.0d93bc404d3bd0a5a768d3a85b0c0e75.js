1cc7e2db042dd4e57b9507cb944848325975381d
shra26 <tvsg26@gmail.com>
2019-08-01 15:43:45 -0400

    Updates

---
+++ precache-manifest.0d93bc404d3bd0a5a768d3a85b0c0e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445ead150a31a6f343456106e1892479",
+    "url": "/lock-key-apparel/index.html"
+  },
+  {
+    "revision": "73c2665ec492988f7a6e",
+    "url": "/lock-key-apparel/static/css/main.c6c1e9e7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/lock-key-apparel/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "73c2665ec492988f7a6e",
+    "url": "/lock-key-apparel/static/js/main.61ee2554.chunk.js"
+  },
+  {
+    "revision": "39447bd252848b95df44",
+    "url": "/lock-key-apparel/static/js/runtime~main.56c76c8c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lock-key-apparel/precache-manifest.0d93bc404d3bd0a5a768d3a85b0c0e75.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lock-key-apparel/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
