2d018a3a9c9e939c10787a3d4d9f389085553ef2
shra26 <tvsg26@gmail.com>
2019-08-01 19:33:38 -0400

    update menu-item with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/homepage.component';
-function App() {
+
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
+
+const App = () => {
 	return (
+		// <HomePage /> //gh-pages github deployment issue with react-router
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
-}
+};
 
 export default App;
