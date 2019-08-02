ce86b4e0e46a366f38de0590b424120027928b29
shra26 <tvsg26@gmail.com>
2019-08-01 21:05:42 -0400

    done: header component

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Implementation from
 Udemy: complete-react-developer-zero-to-mastery
 
 By Andrei Neagoie and Yihua Zhang
+
+Icons made by Freepik from www.flaticon.com is licensed by CC 3.0 BY
--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 const App = () => {
 	return (
 		// <HomePage /> //gh-pages github deployment issue with react-router
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
