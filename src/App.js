6fa245b0765a69f97196448a4249d1ab32e172fc
shra26 <tvsg26@gmail.com>
2019-08-03 02:52:54 -0400

    sign-in completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 const App = () => {
 	return (
 		// <HomePage /> //gh-pages github deployment issue with react-router
@@ -12,6 +14,7 @@ const App = () => {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './custom-button.styles.scss';
+
+export default ({ children, ...otherProps }) => (
+	<button className="custom-button" {...otherProps}>
+		{children}
+	</button>
+);
