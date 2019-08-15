6afdcd9161fe03d4b28c0c3c60b6f303caaba883
shra26 <tvsg26@gmail.com>
2019-08-15 00:17:34 -0400

    firebase,signin page/button added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,45 @@ import HomePage from './pages/HomePage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-const App = () => {
-	return (
-		// <HomePage /> //gh-pages github deployment issue with react-router
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignInAndSignUpPage} />
-			</Switch>
-		</div>
-	);
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentUser: null
+		};
+	}
+
+	unsubscribeFromAuth = null
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({
+				currentUser: user
+			});
+			console.log(user);
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth()
+	}
+
+	render() {
+		return (
+			// <HomePage /> //gh-pages github deployment issue with react-router
+			<div>
+				<Header currentUser={this.state.currentUser}/>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
+}
 
 export default App;
