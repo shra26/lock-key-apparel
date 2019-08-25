1664ba2c6339442a04c025ca3f405f7106071943
shra26 <tvsg26@gmail.com>
2019-08-25 01:06:58 -0400

    sign-up page added

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,7 +4,6 @@ import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
 	<div className="group">
-		{console.log(label)}
 		<input type="text" className="form-input" onChange={handleChange} {...otherProps} />
 		{label ? (
 			<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label `}>
