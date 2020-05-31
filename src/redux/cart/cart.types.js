80c1cd14948847ea7218283bd566e0c1dbdfab10
shra26 <tvsg26@gmail.com>
2020-05-31 14:48:32 -0400

    added cart icon and cartDropdown, cart reducer

---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CartACtionsTypes = {
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+};
+
+export default CartACtionsTypes;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
 
-import rootReducer from './root-reducer';
-import { create } from 'domain';
+import rootReducer from "./root-reducer";
+// import { create } from 'domain';
 
 const middlewares = [logger];
 
